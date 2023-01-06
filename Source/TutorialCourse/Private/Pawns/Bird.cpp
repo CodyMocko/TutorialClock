c9ef4cf05c94a3499ed010aad1ddb715c76e55bf
Cody Mocko <cody.mocko@yahoo.com>
2023-01-05 22:58:46 -0800

    Added basics for advanced inputs onto the bird pawn

--- Source/TutorialCourse/Private/Pawns/Bird.cpp
+++ Source/TutorialCourse/Private/Pawns/Bird.cpp
@@ -2,6 +2,11 @@
 
 
 #include "Pawns/Bird.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h" // Need to go into the build files and add the needed module...if there is still an error you need to rebuild the saved, intermediate, and binary files
 
 // Sets default values
 ABird::ABird()
@@ -10,14 +15,37 @@ ABird::ABird()
 	PrimaryActorTick.bCanEverTick = true;
 
 	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+	Capsule->SetCapsuleHalfHeight(20.f);
+	Capsule->SetCapsuleRadius(15.f);
 	SetRootComponent(Capsule);
+
+	BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BirdMesh"));
+	BirdMesh->SetupAttachment(GetRootComponent());
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 
 // Called when the game starts or when spawned
 void ABird::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	 if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	 {
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(BirdMappingContext, 0);
+		}
+	 }
+}
+
+void ABird::Move(const FInputActionValue& Value)
+{
+	const bool CurrentValue = Value.Get<bool>();
+	if (CurrentValue)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("IA_Move triggered"));
+	}
 }
 
 void ABird::Tick(float DeltaTime)
@@ -31,5 +59,11 @@ void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABird::Move);
+	}
+
+	// PlayerInputComponent->BindAxis(FName("MoveForward"), this, &ABird::MoveForward); Old method
 }
 
--- Source/TutorialCourse/Public/Pawns/Bird.h
+++ Source/TutorialCourse/Public/Pawns/Bird.h
@@ -2,10 +2,16 @@
 
 #pragma once
 
-#include "Components/CapsuleComponent.h"
+
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "InputActionValue.h" // Typically safe to include include files for constructors
 #include "Bird.generated.h" // This needs to be the FINAL include
+
+class UCapsuleComponent;
+class USkeletalMeshComponent;
+class UInputAction;
+class UInputMappingContext;
 
 UCLASS()
 class TUTORIALCOURSE_API ABird : public APawn
@@ -20,11 +26,24 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
 protected:
 	virtual void BeginPlay() override;
+	void MoveForward(float Value);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputMappingContext* BirdMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* MoveAction;
+
+	void Move(const FInputActionValue& Value);
 
 private:
 	// Capsule Component for the Bird
 	UPROPERTY(VisibleAnywhere)
-	UCapsuleComponent* Capsule;
+	UCapsuleComponent* Capsule; // Must forward declare this since the header file for it is in the .cpp files
+
+	UPROPERTY(VisibleAnywhere)
+	USkeletalMeshComponent* BirdMesh; // This is no longer the reccommended way to declare pointers in the header file
 };
